d4d5de7387d42a91b99a8337744926f9aa631e06
Katherine Fortin <kafortin@student.42quebec.com>
2023-02-20 16:43:13 -0500

    norminetted

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kafortin <kafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 15:57:05 by kafortin          #+#    #+#             */
-/*   Updated: 2023/02/16 19:06:12 by kafortin         ###   ########.fr       */
+/*   Updated: 2023/02/20 16:39:22 by kafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,36 +39,42 @@ void	child_two(char **argv, int argc, t_files *files, char **env)
 	free_struct(cmd);
 }
 
+void	command_loop(int argc, char **argv, char **env, t_files *files)
+{
+	int	i;
+
+	i = 2;
+	while (i < argc - 2)
+	{
+		if (pipe(files->fd) < 0)
+			exit_error("Error: pipe did not work");
+		files->pid1 = fork();
+		if (files->pid1 == -1)
+			exit_error("Error: fork did not work");
+		else if (files->pid1 == 0)
+			child_one(argv, i, files, env);
+		else
+		{
+			close(files->fd[1]);
+			dup2(files->fd[0], STDIN_FILENO);
+			close(files->fd[0]);
+		}
+		i++;
+	}
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	t_files	files;
-	int		i;
 
 	if (argc < 5)
 		exit_error("Invalid number of arguments.");
-	i = 2;
 	files.input = open(argv[1], O_RDONLY);
 	if (files.input < 0)
 		exit_error("Error: file could not be opened");
 	dup2(files.input, STDIN_FILENO);
 	close(files.input);
-	while (i < argc - 2)
-	{
-		if (pipe(files.fd) < 0)
-			exit_error("Error: pipe did not work");
-		files.pid1 = fork();
-		if (files.pid1 == -1)
-			exit_error("Error: fork did not work");
-		else if (files.pid1 == 0)
-			child_one(argv, i, &files, env);
-		else
-		{
-			close(files.fd[1]);
-			dup2(files.fd[0], STDIN_FILENO);
-			close(files.fd[0]);
-		}
-		i++;
-	}
+	command_loop(argc, argv, env, &files);
 	child_two(argv, argc, &files, env);
 	waitpid(files.pid1, NULL, 0);
 	return (0);
