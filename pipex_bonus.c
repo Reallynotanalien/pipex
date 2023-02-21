cb3903c45085de5af7b881673d38259305dfdf16
Katherine Fortin <kafortin@student.42quebec.com>
2023-02-21 18:38:00 -0500

    here_doc

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,13 +6,14 @@
 /*   By: kafortin <kafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 15:57:05 by kafortin          #+#    #+#             */
-/*   Updated: 2023/02/20 17:16:09 by kafortin         ###   ########.fr       */
+/*   Updated: 2023/02/21 18:37:16 by kafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 #include <signal.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 void	child_one(char **argv, int i, t_files *files, char **env)
 {
@@ -50,6 +51,8 @@ void	command_loop(int argc, char **argv, char **env, t_files *files)
 	int	i;
 
 	i = 2;
+	if (ft_strncmp("here_doc", argv[1], 8) == 0)
+		i = 3;
 	while (i < argc - 2)
 	{
 		if (pipe(files->fd) < 0)
@@ -69,21 +72,47 @@ void	command_loop(int argc, char **argv, char **env, t_files *files)
 	}
 }
 
+void	validate_heredoc(char **argv, int argc, t_files *files)
+{
+	char	*str;
+
+	if (ft_strncmp("here_doc", argv[1], 8) == 0)
+	{
+		if (argc < 6)
+			exit_error("Invalid number of arguments.");
+		files->input = open(".here_doc", O_TRUNC | O_CREAT | O_RDWR);
+		while (get_next_line(0, &str))
+		{
+			if ((ft_strlen(str) == ft_strlen(argv[2]) + 1)
+				&& (ft_strncmp(str, argv[2], ft_strlen(argv[2])) == 0))
+				break ;
+			else
+				write(files->input, str, ft_strlen(str));
+		}
+	}
+	else
+	{
+		files->input = open(argv[1], O_RDONLY);
+		if (files->input < 0)
+			exit_error("Error: file could not be opened");
+	}
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	t_files	files;
 
 	if (argc < 5)
 		exit_error("Invalid number of arguments.");
+	validate_heredoc(argv, argc, &files);
 	files.output = open(argv[argc - 1], O_TRUNC | O_CREAT | O_WRONLY, 0644);
 	if (files.output < 0)
 		exit_error("Error: file could not be opened/created");
-	files.input = open(argv[1], O_RDONLY);
-	if (files.input < 0)
-		exit_error("Error: file could not be opened");
 	dup2(files.input, STDIN_FILENO);
 	close(files.input);
 	command_loop(argc, argv, env, &files);
+	if (ft_strncmp("here_doc", argv[1], 8) == 0)
+		unlink("./.here_doc");
 	child_two(argv, argc, &files, env);
 	waitpid(files.pid1, NULL, 0);
 	return (0);
