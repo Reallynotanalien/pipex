e4323152469d5dc8faa56e16c5e2ecdfb1376c1a
Katherine Fortin <kafortin@student.42quebec.com>
2023-02-22 17:14:56 -0500

    here_doc works??

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kafortin <kafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 15:57:05 by kafortin          #+#    #+#             */
-/*   Updated: 2023/02/21 18:37:16 by kafortin         ###   ########.fr       */
+/*   Updated: 2023/02/22 17:11:12 by kafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,27 +75,28 @@ void	command_loop(int argc, char **argv, char **env, t_files *files)
 void	validate_heredoc(char **argv, int argc, t_files *files)
 {
 	char	*str;
+	int		temp_file;
 
 	if (ft_strncmp("here_doc", argv[1], 8) == 0)
 	{
 		if (argc < 6)
 			exit_error("Invalid number of arguments.");
-		files->input = open(".here_doc", O_TRUNC | O_CREAT | O_RDWR);
+		temp_file = open("here_doc", O_TRUNC | O_CREAT | O_WRONLY, 0644);
 		while (get_next_line(0, &str))
 		{
 			if ((ft_strlen(str) == ft_strlen(argv[2]) + 1)
 				&& (ft_strncmp(str, argv[2], ft_strlen(argv[2])) == 0))
 				break ;
 			else
-				write(files->input, str, ft_strlen(str));
+				write(temp_file, str, ft_strlen(str));
 		}
+		close(temp_file);
+		files->input = open("here_doc", O_RDONLY);
 	}
 	else
-	{
 		files->input = open(argv[1], O_RDONLY);
-		if (files->input < 0)
-			exit_error("Error: file could not be opened");
-	}
+	if (files->input < 0)
+		exit_error("Error: file could not be opened");
 }
 
 int	main(int argc, char **argv, char **env)
@@ -111,9 +112,9 @@ int	main(int argc, char **argv, char **env)
 	dup2(files.input, STDIN_FILENO);
 	close(files.input);
 	command_loop(argc, argv, env, &files);
-	if (ft_strncmp("here_doc", argv[1], 8) == 0)
-		unlink("./.here_doc");
 	child_two(argv, argc, &files, env);
 	waitpid(files.pid1, NULL, 0);
+	if (ft_strncmp("here_doc", argv[1], 8) == 0)
+		unlink("here_doc");
 	return (0);
 }
